d6d3d32d0e803926f1026c96980d6c698a61b849
yuraSpivak1994 <yura.spivak94@gmail.com>
2019-09-12 19:49:47 +0300

    add filter deley refactor datepikers show in color status, page history-payments

--- src/app/payments/payment-history/payment-history.component.ts
+++ src/app/payments/payment-history/payment-history.component.ts
@@ -1,10 +1,10 @@
-import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CoreService } from "../../core/core.service";
 import { fadeInAnimation } from "../../shared/animation";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
-import { merge, Observable, of as observableOf } from "rxjs";
-import { catchError, map, startWith, switchMap } from "rxjs/operators";
+import { merge, Observable, of as observableOf, Subject } from "rxjs";
+import { catchError, debounceTime, distinctUntilChanged, map, startWith, switchMap } from "rxjs/operators";
 import { PaymentsService } from "../payments.service";
 import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
@@ -20,7 +20,7 @@ import { environment } from "../../../environments/environment";
 export class PaymentHistoryComponent implements OnInit, AfterViewInit {
   toppingList: string[] = ['Show All', 'Successful', 'Unsuccessful'];
   searchValue: string;
-  isOpenSearch =  false;
+  isOpenSearch = false;
   isOpenConfig = false;
   form: FormGroup;
   displayedColumns: string[] = ['Date', 'Time', 'Pay  #', 'Paid by', 'Type', 'Amount (CAD$)', 'Status', 'menu'];
@@ -29,7 +29,16 @@ export class PaymentHistoryComponent implements OnInit, AfterViewInit {
   data: any = [];
   isLoadingResults = true;
   isRateLimitReached = false;
-  minDate = new Date();
+  sortCheckbox = '';
+  startDateSort: string;
+  endDateSort: string;
+  endDateCloseIcon = false;
+  startDateCloseIcon = false;
+  newData: any = [];
+  searchName = '';
+  model: string;
+  modelChanged: Subject<string> = new Subject<string>();
+
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
@@ -39,16 +48,29 @@ export class PaymentHistoryComponent implements OnInit, AfterViewInit {
               private paymentsService: PaymentsService,
               private formBuilder: FormBuilder,
               private _httpClient: HttpClient) {
+
+    this.modelChanged
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((model) => {
+        this.model = model;
+        this.searchName = model
+        this.getQuestion();
+      });
+  }
+
+  changed(text: string) {
+    this.modelChanged.next(text);
   }
 
   getSortData() {
     const sortArray = {
       currentPage: this.paginator.pageIndex + 1,
       itemsPerPage: this.paginator.pageSize,
+      name: this.searchName,
       filter: {
-        endDate: this.checkDateEnd(),
-        startDate: this.checkDateStart(),
-        status: this.checkSortCheckbox()
+        endDate: this.endDateSort,
+        startDate: this.startDateSort,
+        status: this.sortCheckbox
       },
       sort: {}
     };
@@ -56,39 +78,71 @@ export class PaymentHistoryComponent implements OnInit, AfterViewInit {
     return sortArray
   }
 
+  private get statusMap(): any {
+    return {
+      [1]: {
+        color: '#00B274',
+        text: 'Confirmed'
+      },
+      [0]: {
+        color: '#B00020',
+        text: 'Failed'
+      }
+    };
+  }
+
+  private transformStatus(numbers) {
+    return numbers.map((item: any) => ({...item, status: this.statusMap[item.PAYMENT_STATUS]}));
+  }
+
+  showCloseIconFieldStart(value) {
+    this.startDateCloseIcon = !!value;
+  }
+
+  showCloseIconFieldEnd(value) {
+    this.endDateCloseIcon = !!value;
+  }
+
+  clearValueFieldEnd() {
+    this.form.get('endDate').setValue('')
+  }
+
+  clearValueFieldStart() {
+    this.form.get('startDate').setValue('')
+  }
 
   checkDateStart() {
-    let calendarDate = this.form.get('startDate').value;
-    if (calendarDate) {
-      let year = calendarDate.getFullYear().toString();
-      let month = ('0' + (calendarDate.getMonth()+1)).slice(-2);
-      let day =  ('0' + calendarDate.getDate()).slice(-2);
-      let cutDate = year + '-' + month + '-' + day;
-      return cutDate
-    }else  {
-      return '2000-01-01'
-    }
+    this.form.get('startDate').valueChanges.subscribe((value) => {
+      let calendarDate = value;
+      if (calendarDate) {
+        let year = calendarDate.getFullYear().toString();
+        let month = ('0' + (calendarDate.getMonth() + 1)).slice(-2);
+        let day = ('0' + calendarDate.getDate()).slice(-2);
+        this.startDateSort = year + '-' + month + '-' + day;
+        this.getQuestion();
+      } else {
+        this.startDateSort = '2000-01-01';
+        this.getQuestion();
+      }
+      this.showCloseIconFieldStart(value);
+    });
   }
 
   checkDateEnd() {
-    let calendarDate  = this.form.get('endDate').value;
-    if (calendarDate) {
-      let year = calendarDate.getFullYear().toString();
-      let month = ('0' + (calendarDate.getMonth()+1)).slice(-2);
-      let day =  ('0' + calendarDate.getDate()).slice(-2);
-      let cutDate = year + '-' + month + '-' + day;
-      return cutDate
-    } else  {
-      return '3000-01-01'
-    }
-  }
-
-  checkStatus(number) {
-    if (number === 1) {
-      return 'Confirmed'
-    } else if (number === 0) {
-      return 'Failed'
-    }
+    this.form.get('endDate').valueChanges.subscribe((value) => {
+      let calendarDate = value;
+      if (calendarDate) {
+        let year = calendarDate.getFullYear().toString();
+        let month = ('0' + (calendarDate.getMonth() + 1)).slice(-2);
+        let day = ('0' + calendarDate.getDate()).slice(-2);
+        this.endDateSort = year + '-' + month + '-' + day;
+        this.getQuestion();
+      } else {
+        this.endDateSort = '3000-01-01';
+        this.getQuestion();
+      }
+      this.showCloseIconFieldEnd(value);
+    });
   }
 
   initForm() {
@@ -96,7 +150,11 @@ export class PaymentHistoryComponent implements OnInit, AfterViewInit {
       startDate: new FormControl(null),
       endDate: new FormControl(null),
       sort: new FormControl(null),
+      search:  new FormControl(null)
     });
+    this.checkSortCheckboxes();
+    this.checkDateStart();
+    this.checkDateEnd();
   }
 
   getQuestion() {
@@ -112,7 +170,7 @@ export class PaymentHistoryComponent implements OnInit, AfterViewInit {
         map(data => {
           this.isLoadingResults = false;
           this.isRateLimitReached = false;
-          this.resultsLength = data.total_count;
+          this.resultsLength = data.total;
           return data.payments;
         }),
         catchError(() => {
@@ -121,28 +179,31 @@ export class PaymentHistoryComponent implements OnInit, AfterViewInit {
           return observableOf([]);
         })
       ).subscribe((data) => {
-      console.log(this.data = data);
+      this.data = data;
+      this.transformStatus(this.data);
+      this.newData = this.transformStatus(this.data);
     });
   }
-  ngAfterViewInit() {
-    this.getQuestion();
-  }
 
-  checkSortCheckbox() {
-    let sort = 0;
+  checkSortCheckboxes() {
     this.form.get('sort').valueChanges.subscribe(
       value => {
-        if (value === 'Show All') {
-          return sort = 0
-        } else if (value === 'Successful') {
-         return  sort = 1
-        } else if (value === 'Unsuccessful') {
-         return sort = 0
+        if (value == "Show All") {
+          this.sortCheckbox = '';
+          this.getQuestion();
+        } else if (value == 'Successful') {
+          this.sortCheckbox = '1';
+          this.getQuestion();
+        } else if (value == 'Unsuccessful') {
+          this.sortCheckbox = '0';
+          this.getQuestion();
         }
       }
     );
-    return sort
+  }
 
+  ngAfterViewInit() {
+    this.getQuestion();
   }
 
   ngOnInit() {
@@ -151,7 +212,7 @@ export class PaymentHistoryComponent implements OnInit, AfterViewInit {
 
 
   clearSearch() {
-    this.searchValue = '';
+    this.model = '';
   }
 
   toggleSearch() {
@@ -164,7 +225,8 @@ export class PaymentHistoryComponent implements OnInit, AfterViewInit {
 }
 
 export class PaymentsHttpDatabase {
-  constructor(private _httpClient: HttpClient) {}
+  constructor(private _httpClient: HttpClient) {
+  }
 
   getRepoIssues(sortArray): Observable<any> {
     const requestUrl =
